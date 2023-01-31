69641e7de75f88df74389d1435747c94ce09322d
Visualize-ML <105787223+Visualize-ML@users.noreply.github.com>
2023-01-31 09:51:32 -0500

    Add files via upload

--- Book3_Ch02_Python_Codes/Bk3_Ch2_02.py
+++ Book3_Ch02_Python_Codes/Bk3_Ch2_02.py
@@ -16,10 +16,10 @@ factorial = 1
 
 # check if the number is negative, positive or zero
 if num < 0:
-   print("Factorial does not exist for negative numbers")
+    print("Factorial does not exist for negative numbers")
 elif num == 0:
-   print("The factorial of 0 is ", factorial)
+    print("The factorial of 0 is ", factorial)
 else:
-   for i in range(1,num + 1):
+    for i in range(1,num + 1):
        factorial = factorial*i
-   print("The factorial of",num," is ",factorial)
+    print("The factorial of",num," is ",factorial)
